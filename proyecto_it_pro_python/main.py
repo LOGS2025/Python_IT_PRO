1c33ce128c502f09393a45c2929c5e2279c9d6dc
sebas <225sebas@gmail.com>
2026-03-02 22:17:46 -0600

    Planning on parsing and moving to main.py

--- proyecto_it_pro_python/main.py
+++ proyecto_it_pro_python/main.py
@@ -10,20 +10,53 @@ Guardar porcentajes de costos y variables de precios. Como JSON, CSV y XML
 3. Gráficas.
 '''
 
-import json
+dimensiones_financiamiento = 'organismo,grupo_organismo,rango_salarial,valor_vivienda,tipo_credito,modalidad'
+dimensiones_conavi = 'rango_edad,rango_salarial,tipo_vivienda,valor_vivienda,modalidad'
+dimensiones_FOVISSSTE = 'rango_edad,rango_salarial,valor_vivienda,modalidad'
+dimensiones_infonavit = 'rango_edad,rango_salarial,valor_vivienda,vivienda,modalidad'
+dimensiones_CNBV = 'rango_edad,rango_salarial,valor_vivienda,modalidad'
+dimensiones_inv_vivienda = 'avance_obra,segmento_uma,tipo_vivienda,pcu,segmento'
+dimensiones_reg_vivienda = 'segmento,segmento_uma,tipo_vivienda,superficie,recamara'
+dimensiones_verificacion = 'segmento,segmento_uma,tipo_vivienda,superficie,recamara'
+dimensiones_produccion = 'segmento,segmento_uma,tipo_vivienda,superficie,recamara'
+
+
 import pandas as pd
+import gobCSV_housing as ghCSV
+import json
 
 
-def calc_costos_construccion():
+def housing_costs():
+    segmento_uma : str
+    segmento : str
+    superficie : str
+    recamara : str
+    pcu : str
+    tipo_vivienda : str
+    modalidad : str
+    valor_vivienda : str 
+    avance_obra : str
+    pass
+def demographic_parse():
+    rango_edad : str
+    rango_salarial : str
     pass
 
+# PARSE DATAFRAMES ON HOUSING COSTS 
+def calc_costos_construccion():
+    housing_costs()
+    pass
+
+# MATH
 def costo_final():
     pass
 
+# MATH
 def ganancia_total():
     pass
 
 def ofertaDemanda():
+    demographic_parse()
     pass
 
 def incertidumbre():
@@ -31,5 +64,17 @@ def incertidumbre():
 
 
 
+############# START OF API CSV GENERATION #########
+Financiamiento = ghCSV.API_gob_vivienda_builder("GetFinanciamiento",dimensiones_financiamiento)
+CONAVI = ghCSV.API_gob_vivienda_builder("GetCONAVI",dimensiones_conavi)
+FOVISSSTE = ghCSV.API_gob_vivienda_builder("GetFOVISSSTE",dimensiones_FOVISSSTE)
+INFONAVIT = ghCSV.API_gob_vivienda_builder("GetINFONAVIT",dimensiones_infonavit)
+CNBV = ghCSV.API_gob_vivienda_builder("GetCNBV",dimensiones_CNBV)
+Inventario_de_vivienda = ghCSV.API_gob_vivienda_builder("GetInventario",dimensiones_inv_vivienda)
+Registro_de_vivienda = ghCSV.API_gob_vivienda_builder("GetRegistro",dimensiones_reg_vivienda)
+Verificacion_de_vivienda = ghCSV.API_gob_vivienda_builder("GetVerificacion",dimensiones_verificacion)
+Produccion_de_vivienda = ghCSV.API_gob_vivienda_builder("GetProduccion",dimensiones_produccion)
+############ END OF API CSV GENERATION #########
 
-
+'''
+'''

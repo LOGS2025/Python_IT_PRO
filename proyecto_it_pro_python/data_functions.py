a73f3b20ade2fc58c682a79ebc6f7dee743ec463
sebas <225sebas@gmail.com>
2026-03-11 17:12:56 -0500

    Data file for data functions

---
+++ proyecto_it_pro_python/data_functions.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Media por Grupo Salarial
+def average(matriz):
+    print("Media por Grupo Salarial :",matriz.mean(axis=1))
+    print("Media por Año :",matriz.mean(axis=0))
+    pass    
+'''
+- Higher value → **more unstable price growth**
+- Lower value → **more stable market**
+'''
+def volatility(matriz):
+    volatility = matriz.std(axis=1)
+    print("volatility\n\n",volatility)
+    pass
+'''
+Long-Term Growth by Income Group
+'''
+def growth(matriz):
+    growth = matriz.iloc[:,-1] - matriz.iloc[:,0]
+    print("Growth Matrix: \n\n",growth)
+    print("Grupo Salarial con mayor crecimiento de Media del monto/vivienda nueva cada año :",matriz.idxmax())
+    print("Grupo Salarial con mayor decrecimiento de Media del monto/vivienda nueva cada año :",matriz.idxmin())
+
+    pass
+'''
+Mapa de Calor
+'''
+def heatmap(matriz):
+    sns.heatmap(matriz, cmap="coolwarm", center=0)
+    plt.show()
+    pass
